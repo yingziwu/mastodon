ea725f5a62f0ca54d97481a368d918604116a102
bgme <i@bgme.me>
2025-04-12 22:46:10 +0800

    fix merge bug

--- app/validators/poll_options_validator.rb
+++ app/validators/poll_options_validator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class PollValidator < ActiveModel::Validator
+class PollOptionsValidator < ActiveModel::Validator
   MAX_OPTIONS      = 12
   MAX_OPTION_CHARS = 50
 
