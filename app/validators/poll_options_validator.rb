ec756c5c4720993f6c2b26055fa746e9e49fbfa6
bgme <i@bgme.me>
2026-01-31 00:13:25 +0800

    Add MAX_POLL_OPTIONS

--- app/validators/poll_options_validator.rb
+++ app/validators/poll_options_validator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PollOptionsValidator < ActiveModel::Validator
-  MAX_OPTIONS      = 4
+  MAX_OPTIONS      = (ENV['MAX_POLL_OPTIONS'] || 4).to_i
   MAX_OPTION_CHARS = 50
 
   def validate(poll)
